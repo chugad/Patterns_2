f106f44ea2854ddf961c0def7a6fdc506f1f553a
Oleg Dudin <o.v.dudin@yandex.ru>
2020-11-19 19:06:23 +0700

    Initial commit

--- src/test/java/ru/netology/AuthorizationTest.java
+++ src/test/java/ru/netology/AuthorizationTest.java
@@ -1,50 +1,52 @@
 package ru.netology;
 
-
-import com.google.gson.Gson;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static io.restassured.RestAssured.given;
 
 public class AuthorizationTest {
 
-    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
+    static UserGenerator.CreateUser activeUser = UserGenerator.getCreateActiveUser();
+    static UserGenerator.CreateUser blockedUser = UserGenerator.getCreateBlockedUser();
 
-    @BeforeAll
-    static void setUpAll() {
-
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new Gson().toJson(UserGenerator.getAuthInfo())) // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
+    @Test
+    void happyPath() {
+        UserGenerator.setUpActiveUser();
+        open("http://localhost:9999");
+        $("[data-test-id=login] input").setValue(activeUser.getLogin());
+        $("[data-test-id=password] input").setValue(activeUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $$("h2").findBy(text("Личный кабинет")).shouldBe(visible);
     }
 
     @Test
-    void test1() {
+    void shouldNotSussesLoginActiveUser_SameNameAnotherPassword() {
+        UserGenerator.setUpActiveUser();
         open("http://localhost:9999");
-        $("[data-test-id=login] input").setValue(UserGenerator.getAuthInfo().getLogin());
-        $("[data-test-id=password] input").setValue(UserGenerator.getAuthInfo().getPassword());
+        $("[data-test-id=login] input").setValue(activeUser.getLogin());
+        $("[data-test-id=password] input").setValue(activeUser.getPassword2());
         $("[data-test-id='action-login']").click();
-        $$("h2").findBy(text("Личный кабинет")).shouldBe(visible);
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldNotSussesLoginActiveUser_AnotherNameSamePassword() {
+        UserGenerator.setUpActiveUser();
+        open("http://localhost:9999");
+        $("[data-test-id=login] input").setValue(activeUser.getLogin2());
+        $("[data-test-id=password] input").setValue(activeUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldNotSussesLoginBlockedUser() {
+        UserGenerator.setUpBlockedUser();
+        open("http://localhost:9999");
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Пользователь заблокирован")).shouldBe(visible);
     }
 }
--- src/test/java/ru/netology/UserGenerator.java
+++ src/test/java/ru/netology/UserGenerator.java
@@ -1,29 +1,87 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import lombok.Value;
 
-public class UserGenerator {
-    private UserGenerator() {}
+import static io.restassured.RestAssured.given;
+
+
+public abstract class UserGenerator {
+    private static Faker faker = new Faker();
+    private static final String login = faker.name().username();
+    private static final String password = faker.internet().password();
+
+
+    private UserGenerator() {
+    }
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static CreateUser getCreateActiveUser() {
+        CreateUser activeUser = new CreateUser(login, password, "active");
+        return activeUser;
+    }
+
+    static void setUpActiveUser() {
+
+        // сам запрос
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new Gson().toJson(getCreateActiveUser()))// передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    public static CreateUser getCreateBlockedUser() {
+        CreateUser blockedUser = new CreateUser("qwerty", "password1", "blocked");
+        return blockedUser;
+    }
+
+    static void setUpBlockedUser() {
+
+        // сам запрос
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new Gson().toJson(getCreateBlockedUser()))// передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
 
     @Value
-    public static class AuthInfo {
-        private String login;
-        private String password;
-        private String status;
+    public static class CreateUser {
+        String login;
+        String password;
+        String status;
 
         public String getLogin() {
             return login;
         }
 
+        public String getLogin2() {
+            return faker.name().username();
+        }
+
         public String getPassword() {
             return password;
         }
+
+        public String getPassword2() {
+            return faker.internet().password();
+        }
     }
-
-    public static AuthInfo getAuthInfo() {
-        return new AuthInfo("vasya", "qwerty123", "active");
-    }
-
-
 }
