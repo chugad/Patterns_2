4c9ad24e6534ca5c3effd58c18d90c5a9142b367
Oleg Dudin <o.v.dudin@yandex.ru>
2020-11-24 19:38:21 +0700

    Refactor code

--- src/test/java/ru/netology/AuthorizationTest.java
+++ src/test/java/ru/netology/AuthorizationTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIConversion;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -7,45 +8,43 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class AuthorizationTest {
 
-    static UserGenerator.CreateUser activeUser = UserGenerator.getCreateActiveUser();
-    static UserGenerator.CreateUser blockedUser = UserGenerator.getCreateBlockedUser();
-
     @Test
     void happyPath() {
-        UserGenerator.setUpActiveUser();
+        UserInfo.User.setUpActiveUser();
+
         open("http://localhost:9999");
-        $("[data-test-id=login] input").setValue(activeUser.getLogin());
-        $("[data-test-id=password] input").setValue(activeUser.getPassword());
+        $("[data-test-id=login] input").setValue(UserInfo.User.getLogin());
+        $("[data-test-id=password] input").setValue(UserInfo.User.getPassword());
         $("[data-test-id='action-login']").click();
         $$("h2").findBy(text("Личный кабинет")).shouldBe(visible);
     }
 
     @Test
     void shouldNotSussesLoginActiveUser_SameNameAnotherPassword() {
-        UserGenerator.setUpActiveUser();
+        UserInfo.User.setUpActiveUser();
         open("http://localhost:9999");
-        $("[data-test-id=login] input").setValue(activeUser.getLogin());
-        $("[data-test-id=password] input").setValue(activeUser.getPassword2());
+        $("[data-test-id=login] input").setValue(UserInfo.User.getLogin());
+        $("[data-test-id=password] input").setValue(UserInfo.User.getPassword2());
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль")).shouldBe(visible);
     }
 
     @Test
     void shouldNotSussesLoginActiveUser_AnotherNameSamePassword() {
-        UserGenerator.setUpActiveUser();
+        UserInfo.User.setUpActiveUser();
         open("http://localhost:9999");
-        $("[data-test-id=login] input").setValue(activeUser.getLogin2());
-        $("[data-test-id=password] input").setValue(activeUser.getPassword());
+        $("[data-test-id=login] input").setValue(UserInfo.User.getLogin2());
+        $("[data-test-id=password] input").setValue(UserInfo.User.getPassword());
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль")).shouldBe(visible);
     }
 
     @Test
     void shouldNotSussesLoginBlockedUser() {
-        UserGenerator.setUpBlockedUser();
+        UserInfo.User.setUpBlockedUser();
         open("http://localhost:9999");
-        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
-        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("[data-test-id=login] input").setValue(UserInfo.User.getLogin());
+        $("[data-test-id=password] input").setValue(UserInfo.User.getPassword());
         $("[data-test-id='action-login']").click();
         $("[data-test-id=error-notification] .notification__content").shouldHave(text("Пользователь заблокирован")).shouldBe(visible);
     }
